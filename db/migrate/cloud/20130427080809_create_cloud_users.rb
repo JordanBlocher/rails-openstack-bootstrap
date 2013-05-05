233b061f2253f4b4f4c9fb828f3d215020182a36
JordanBlocher <jordanblocher@gmail.com>
2013-05-05 10:50:30 -0700

    Gitignore.

--- db/migrate/cloud/20130427080809_create_cloud_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateCloudUsers < ActiveRecord::Migration
-  def change
-    create_table :cloud_users do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/cloud/20130427081122_add_admin_to_cloud_users.rb
+++
@@ -1,9 +0,0 @@
-class AddAdminToCloudUsers < ActiveRecord::Migration
-  def self.up
-    add_column :cloud_users, :admin, :boolean, :default => false
-  end
-
-  def self.down
-    remove_column :cloud_users, :admin
-  end
-end
